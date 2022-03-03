c83c6f3dd8b7f9f5a4aa19cb7a089056b73a6bc6
gdendgithub <giridharreddydendi@gmail.com>
2022-03-03 22:34:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ubuntu" {
   }
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu2" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
